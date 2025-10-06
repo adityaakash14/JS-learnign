cc46c37cd0d010f6642561a5dd09d058561d4527
Akash Aditya <137680797+adityaakash14@users.noreply.github.com>
2025-10-06 17:58:15 +0000

    conversion of data type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "AKASH"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33ABS" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = "akash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "akash" =>true
+
+let someNumber = true
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
