cb6a9c7c44f9b76f41861073f78968f2965ba8b4
Akash Aditya <137680797+adityaakash14@users.noreply.github.com>
2025-10-07 17:54:45 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive
+//  call by value
+// 7  types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100 
+const scoreValue = 100.3
+
+// const isLoggedIn = false
+const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+//  const bigNumber =  343565663632665n
+// console.log(bigNumber);
+
+
+
+
+
+// Reference/Non-Primitive
+
+// Array, Objects, Functions
+
+
+const heros  = ["Shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Akash"
+      
+}
+
+const myFunction = function(){
+    console.log("Akash");
+    
+}
+
+console.log(typeof heros);
+
+
