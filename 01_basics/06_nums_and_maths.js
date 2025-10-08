418a7031ea71e77a51f802b884b081660ac6c21e
Akash Aditya <137680797+adityaakash14@users.noreply.github.com>
2025-10-08 19:46:14 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+const  score = 400
+
+const balance = new Number(100.43)
+
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toFixed(1));
+
+const otherNumber =1123.2966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(4.2));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+// its give values between 0 to 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
+
+
+
+
+
